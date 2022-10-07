6384599cf1759a9c086213a4e95f43eac85c3fba
Ashot Gimishyan <gimishyan.aa@phystech.edu>
2022-10-07 18:51:43 +0300

    update 6

--- 24_Exceptions/06_chat/main.py
+++ 24_Exceptions/06_chat/main.py
@@ -1,27 +1,31 @@
+file = open('history.txt', 'w')
+file.close()
 
+while True:
+    user_name = input("Введите ваше имя: ")
+    print(f"{user_name}, выберите действие:")
+    print("1. Посмотреть текущий текст чата")
+    print("2. Отправить сообщение")
+    ans = input("1 или 2: ")
 
-with open('history.txt', 'w+') as history:
-    while True:
-        user_name = input("Введите ваше имя: ")
-        print(f"{user_name}, выберите действие:")
-        print("1. Посмотреть текущий текст чата")
-        print("2. Отправить сообщение")
-        ans = input("1 или 2: ")
+    try:
+        int(ans)
+    except ValueError:
+        print("Что-то пошло нет так. Начнем сначала")
 
-        try:
-            int(ans)
-        except ValueError:
-            print("Что-то пошло нет так. Начнем сначала")
+    if int(ans) == 1:
+        file = open('history.txt', 'r')
+        for line in file:
+            print(line, end="")
+        print()
+        file.close()
 
-        if int(ans) == 1:
-            for line in history:
-                print(line, end="")
-            print()
+    elif int(ans) == 2:
+        file = open('history.txt', 'a')
+        user_mes = input("Введите ваше сообщение: ")
+        user_mes = f"{user_name}: {user_mes}"
+        print(user_mes, file=file)
+        file.close()
 
-        elif int(ans) == 2:
-            user_mes = input("Введите ваше сообщение: ")
-            user_mes = f"{user_name}: {user_mes}"
-            print(user_mes, file=history)
-
-        else:
-            print("Только 1 или 2!!!")
+    else:
+        print("Только 1 или 2!!!")
